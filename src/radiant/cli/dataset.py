8bd9e8cf17027949ed19d6f3c4ddc4e34c015d3d
Pepe Coral <coritochico@gmail.com>
2026-05-11 23:06:32 +0200

    feat: :sparkles: added basic structure for dataset commands

---
+++ src/radiant/cli/dataset.py
@@ -0,0 +1,21 @@
+import typer
+
+app = typer.Typer()
+
+@app.command()
+def init():
+    pass
+
+
+@app.command()
+def push():
+    passuv 
+
+@app.command(name="list")
+def list_remote():
+    pass
+
+
+@app.command()
+def show():
+    pass
